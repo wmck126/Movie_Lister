a7a83d5bbdd1b67148b807d20a478acbccd93c22
Ward McKinley <wmck126@gmail.com>
2022-05-16 18:35:46 -0400

    Added search/navbar

--- index.js
+++ index.js
@@ -14,10 +14,10 @@ In order to populate info into the cards I need to:
 */
 
 const keepers = []
-const popKeepers = []
+const titleKeepers = []
 
 //Grab info from db
-for(let i =100; i < 150; i++) {
+for(let i =100; i < 105; i++) {
     fetch(`https://api.themoviedb.org/3/movie/${i}?api_key=d4cda60bbc10fd473970ba19b43e2e88&language=en-US`)
     .then(resp => resp.json())
     .then(function(obj){
@@ -124,10 +124,23 @@ for(let i =100; i < 150; i++) {
                         
                     }
             })
-            }
+
+            // //Get recommendations for selected movie
+            // const recommendationsBtn = newCard.querySelector('#recommendations')
             
+            // titleKeepers.push(b.original_title)
+            // //titleKeepers.shift()
+            // console.log(titleKeepers)
+            // recommendations(recommendationsBtn, i)
+
+
+
+
+
+
+
+            }
         )
-        
 }
 //likes the first card displayed, if removed the first card will not be able to be liked
 function likeFirstCard() {
@@ -176,11 +189,125 @@ function descriptionShow(){
                         descripCounter++
                     }
                     else {
-                        descripLocation.style.display = 'none'
-                        descripCounter= 0
-                    }
-            })
+                        descripLocation.style.display = console.log(obj)
+                    }})
 }
 likeFirstCard()
 dislikeFirstCard()
 descriptionShow()
+
+// async function recommendations(recBtn, i){
+//     const secondHeader = document.querySelector('h3')
+//     recBtn.addEventListener('click', function(e){
+//         console.log(e)
+//         secondHeader.textContent = 'Recommendations for: ' + titleKeepers[i]
+//     })
+
+// }
+
+
+document.addEventListener('DOMContentLoaded', () => {
+    document.querySelector('form').addEventListener('submit', (e) =>{
+        e.preventDefault()
+        
+        const keyword = e.target.searchbar.value
+        console.log(keyword)
+        fetch(`https://api.themoviedb.org/3/search/movie?api_key=d4cda60bbc10fd473970ba19b43e2e88&language=en-US&query=${keyword}&page=1&include_adult=false`)
+        .then(resp => resp.json())
+        .then(function (obj){
+            const results = obj.results
+            for(let i =0; i<results.length; i++){
+                const cardArr = []
+                const cardLocation = document.querySelector('.col-auto.mb-3')
+                const newCard = cardLocation.cloneNode(true)
+                newCard.id = `${i}`
+                cardLocation.after(newCard)
+                cardArr.push(newCard)
+
+        //Puts pictures into each new card
+            const cardPicture = results[i].backdrop_path
+            const imgLocation = document.querySelector('.card-img-top')
+            const cardPicArr = [cardPicture]
+                cardPicArr.forEach(function() {
+                    imgLocation.src = 'https://image.tmdb.org/t/p/w300' + cardPicArr
+                    imgLocation.alt = i
+                })
+        //Puts each title from the fetch request into the card
+            const cardTitle = results[i].original_title
+            const titleArr = [cardTitle]
+            const titleClass = document.querySelector('.card-title')
+            titleArr.forEach(function(){
+                titleClass.textContent = cardTitle
+            })
+        //Puts each description from fetch request into the card
+            const cardDescrip = results[i].overview
+            const descripArr = [cardDescrip]
+            const descripClass = document.querySelector('.card-text')
+            descripArr.forEach(function(){
+                descripClass.textContent = cardDescrip
+            })
+        //Puts popularity rating into each card 
+            const cardPop = results[i].popularity
+            const popArr = [cardPop]
+            popKeepers.push(popArr)
+            const popClass = document.querySelector('.popularity')
+            popArr.forEach(function(e){
+                popClass.textContent = 'Popularity rating: ' + e
+            })
+        //deletes unecessary cards with no info populating
+            const emptyTitle = newCard.querySelector('.card-title')
+            if (emptyTitle.innerHTML === 'Card title'){
+                return newCard.style.display= 'none'
+            }
+        //Adds functionality to like and dislike buttons
+            let likeButton = newCard.querySelector('.btn.btn-success')
+            let dislikeButton = newCard.querySelector('.btn.btn-danger')
+            let body = newCard.querySelector('.card')
+            let likeCount = 0
+            //adds and removes highlight class to the cards
+                likeButton.addEventListener('click', function () {
+                    if (likeCount=== 0){
+                        body.classList.add('highlight')
+                        likeCount++
+                    }
+                    else{
+                            body.classList.remove('highlight')
+                            likeCount=0
+                        }
+                })
+            dislikeButton.addEventListener('click', function(seeya){
+                return newCard.remove()
+            })
+        //On mouseclick show card descriptions
+            const descripLocation = newCard.querySelector('.card-text')
+            const cardLocationNoButton = newCard.querySelector('.card-body')
+            const imgLocationNoButton = newCard.querySelector('.card-img-top')
+            let descripCounter = 0
+            imgLocationNoButton.addEventListener('click', function(g){
+                if (descripCounter === 0){
+                    descripLocation.style.display = 'block'
+                    descripCounter++
+                }
+                else {
+                    descripLocation.style.display = 'none'
+                    descripCounter= 0
+                }
+        })
+            cardLocationNoButton.addEventListener('click', function(g){
+                    if (descripCounter === 0){
+                        descripLocation.style.display = 'block'
+                        descripCounter++
+                        
+                    }
+                    else {
+                        descripLocation.style.display = 'none'
+                        descripCounter= 0
+                        
+                    }
+            })
+                }})
+            })
+            }
+            
+        )
+
